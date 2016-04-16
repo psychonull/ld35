ec936aa2a2e753aa0cfd2fc4d588318e373ffbcb
Matias Arriola <matias.arriola@gmail.com>
2016-04-16 18:14:04 -0300

    Add Level Editor

---
+++ src/editor.js
@@ -0,0 +1,110 @@
+// dirty editor for ld35
+
+let $ = window.$;
+let state = {
+  x: null,
+  y: null,
+  start: null,
+  end: null,
+  map: null
+};
+let selectedTool = null;
+
+
+$(document).on('ready', () => {
+  $('#x, #y').on('change', function() {
+    state[this.id] = parseInt($(this).val());
+    onChangeGridSize();
+  });
+
+  $('#grid-container').on('click', 'td', onClickGridCell);
+
+  $('.tool').on('click', function(e){
+    e.preventDefault();
+    selectedTool = parseInt($(this).data('tiletype'));
+    $('.tool').removeClass('btn-primary');
+    $(this).addClass('btn-primary');
+  });
+
+  $('#export').on('click', function(e){
+    e.preventDefault();
+    $('#export-text').val(getLevelFromState(state));
+    $('#export-modal').modal('show');
+  });
+
+  $('#import').on('click', function(e){
+    e.preventDefault();
+    $('#import-modal').modal('show');
+  });
+
+  $('#export-text').on('focus', function(){
+    $(this).select();
+  });
+});
+
+function getLevelFromState(state){
+  let cells = state.map.map((row, j) => {
+    return row.map((cell, i) => {
+      if(!cell){
+        return 0;
+      }
+      if(i === state.start[0] && j === state.start[1]){
+        return cell + 100;
+      }
+      if(i === state.end[0] && j === state.end[1]){
+        return cell + 900;
+      }
+      return cell;
+    });
+  });
+  let data = {
+    gridSize: [state.x, state.y],
+    cells,
+    maxMoves: parseInt($('#maxMoves').val()) || false
+  };
+  return JSON.stringify(data);
+}
+
+function onClickGridCell(){
+  if(selectedTool === null){
+    return;
+  }
+  let [x, y] = $(this).data('location').split('-').map((x) => parseInt(x));
+  if(selectedTool === 100 && !state.map[y][x]){
+    console.warn('map start show be of a type ');
+  }
+  if(selectedTool === 100 || selectedTool === 900){
+    $('#grid-container td').removeClass('opt' + selectedTool);
+    this.className += ' opt' + selectedTool;
+    if(selectedTool === 100){
+      state.start = [x, y];
+    }
+    else {
+      state.end = [x, y];
+    }
+  }
+  else {
+    this.className = 'opt' + selectedTool;
+    state.map[y][x] = selectedTool;
+  }
+}
+
+function onChangeGridSize(){
+  if(!state.x || !state.y){
+    return;
+  }
+  if(!state.map){
+    state.map = [];
+  }
+  let $grid = $('<table>');
+  for(let i = 0; i < state.y; i++){
+    let $row = $('<tr>');
+    state.map[i] = [];
+    for(let j = 0; j < state.x; j++){
+      $row.append(`<td data-location="${j}-${i}">`);
+      state.map[i].push(0);
+    }
+    $grid.append($row);
+  }
+  $('#grid-container').html($grid);
+}
