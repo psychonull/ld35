bb701e964d415eb4930f146bfdbb87a1056cd7c1
Matias Arriola <matias.arriola@gmail.com>
2016-04-17 21:50:20 -0300

    add continue buttons

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,17 +1,32 @@
 import { PropTypes } from 'react';
 
-const startAndClose = (props, isHistory) => {
+const startAndClose = (props, isHistory, isContinue) => {
   document.getElementById('container').classList.remove('hidden');
-  props.game.start(0, {isHistory});
+  if(isContinue){
+    let maxLevel = props.game.store.getState().gameState.maxLevel;
+    props.game.start(maxLevel - 1, {isHistory});
+  }
+  else {
+    props.game.start(0, {isHistory});
+  }
   props.onClose();
 };
 
 const MainMenu = (props) => {
+  let maxLevel = props.game.store.getState().gameState.maxLevel;
   return (
     <div class="main-menu">
       <h1>LD35: Game</h1>
       <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, true);}}><h3>History Mode</h3></a>
+      {
+        maxLevel !== 1 &&
+        <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, true, true);}}><h4>Continue</h4></a>
+      }
       <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, false);}}><h3>Arcade</h3></a>
+      {
+        maxLevel !== 1 &&
+        <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, false, true);}}><h4>Continue</h4></a>
+      }
     </div>
   );
 };
