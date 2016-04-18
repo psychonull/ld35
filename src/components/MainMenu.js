a66d2ab0208f8d2ef5a9deaedd87173665e593c1
Matias Arriola <matias.arriola@gmail.com>
2016-04-18 11:16:06 -0300

    some mainmenu styles

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -16,17 +16,23 @@ const MainMenu = (props) => {
   let maxLevel = props.game.store.getState().gameState.maxLevel;
   return (
     <div class="main-menu">
-      <h1>LD35: Game</h1>
-      <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, true);}}><h3>History Mode</h3></a>
-      {
-        maxLevel !== 1 &&
-        <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, true, true);}}><h4>Continue</h4></a>
-      }
-      <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, false);}}><h3>Arcade</h3></a>
-      {
-        maxLevel !== 1 &&
-        <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, false, true);}}><h4>Continue</h4></a>
-      }
+      <h1 className="title">LD35: Game</h1>
+      <div className="options">
+        <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, true);}}><h3>History Mode</h3></a>
+        {
+          maxLevel !== 1 &&
+          <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, true, true);}}><h4>Continue</h4></a>
+        }
+        <hr />
+        <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, false);}}><h3>Arcade</h3></a>
+        {
+          maxLevel !== 1 &&
+          <a href="" onClick={(e) => {e.preventDefault(); startAndClose(props, false, true);}}><h4>Continue</h4></a>
+        }
+      </div>
+      <div className="footer">
+        Developed by Psychonull for LD35. <a href="https://github.com/psychonull/ld35" target="_blank">Source code</a>.
+      </div>
     </div>
   );
 };
